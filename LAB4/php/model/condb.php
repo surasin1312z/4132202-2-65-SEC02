c645c8a5e98d3eda5a1ff6dcf82eadd2d5547fb9
surasin1312z <640112418073@bru.ac.th>
2022-12-08 12:14:37 +0700

    lab4

---
+++ LAB4/php/model/condb.php
@@ -0,0 +1,15 @@
+<?php
+$host = "db";
+$user = "root";
+$pass = "1234";
+$db = "db_student";
+
+try {
+    $link = mysqli_connect($host,$user,$pass,$db);
+    mysqli_query($link,"SET NAMES utf8");
+} catch (Exception $e) {
+    echo "An error is" . $e . " : : " . mysqli_error($link);
+}
+
+
+?>
